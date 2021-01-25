a1ade6175fc5909f24b57f49715bde5c36e6174b
cvilanova <cyvila@gmail.com>
2021-01-25 11:44:05 -0500

    add reservation name field

--- src/app/core/models/reservation.model.ts
+++ src/app/core/models/reservation.model.ts
@@ -3,6 +3,7 @@ import {Session} from './session.model';
 export class Reservation {
   id_reservation: number;
   id_room: number;
+  name: string;
   reservation_start_datetime: string | Date;
   reservation_end_datetime: string | Date;
   session_uuid: string;
